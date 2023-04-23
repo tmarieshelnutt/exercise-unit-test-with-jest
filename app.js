c9d97f9facc465913ebca872c0e05d0d9d22f483
Tara <tmarieshelnutt@gmail.com>
2023-04-23 02:34:23 +0000

    should be finished....

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-console.log("Hello World")
-
-const sum = (a,b) => {
-    return a + b
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
 }
-console.log(sum(7,3))
 
-module.exports = { sum };
+// we include fromEuroToDollar here as well because it needs to be exported
+module.exports = { sum, fromEuroToDollar }
 
+
+const fromDollarToYen = function(valueInDollar ){
+    
+    let valueInYen = valueInDollar * 127.9;
+    
+    return valueInYen;
+}
+module.exports = { sum, fromDollarToYen }
+
+
+const fromYenToPound = function(valueInYen){
+    
+    let valueInPound = valueInYen * 0.8;
+            
+    return valueInPound;
+}        
+module.exports = { sum, fromYenToPound }
--- test.js
+++ test.js
@@ -1,53 +1,31 @@
-// const { sum } = require('./app.js');
 
-// test ('adds 14 + 9 to equal 23', () => {
-//     let total = sum(14, 9);
-//     expect(total).toBe(23);
-// });
+import fromEuroToDollar
 
-//one euro is:
+const { sum } = require('./app.js');
+
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14, 9);
+    expect(total).toBe(23);
+});
+
+// one euro is:
 let oneEuroIs = {
     "JPY": 127.9,
     "USD": 1.2,
     "GPB": 0.8
 }
+1 EUR (Euro) = 1.2 USD (US Dollar)
 
-//1 EUR (Euro) = 1.2 USD (US Dollar)
-
-function fromDollarToYen(dollars) {
-    const exchangeRate = 127.9;
-    const yen = Dollars * exchangeRate;
-    return yen;
-}
-
-const dollars = 50;
-const yen = fromDollarToYen(dollars)
-console.log('${dollars} dollars=${yen} yen')
-
-function fromEuroToDollar(Euro) {
-    const exchangeRate = 1.2;
-    const dollar = Euro * exchangeRate;
-    return dollar;
-}
-
-const euro = 50;
-const dollar = fromEuroToDollar(dollars)
-console.log('${euro} euro=${dollar} dollar')
-
-
-function fromYenToPound(Yen) {
-    const exchangeRate = 0.8;
-    const pound = Yen * exchangeRate;
-    return pound;
-}
-
-const yen = 50;
-const pound = fromYenToPound(yen)
-console.log('${yen} yen=${pound} pound')
-
-test("One euro should be 1.206 dollars", function() {
+test("One euro should be 1.206 dollars", function(){
+    //import the function from app.js
     const { fromEuroToDollar } = require('./app.js')
+
+    // use the function like its suppoed to be used
     const dollars = fromEuroToDollar(3.5)
-    const expected = 3.5 * 1.2;
-    expect(fromEuroToDollar(3.5)).toBe(4.2);
+
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    const expected = 3.5 * 1.2; 
+    
+    // this is the comparison for the unit test
+     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
